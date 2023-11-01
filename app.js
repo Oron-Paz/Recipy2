46c040836311737b61e26e3d17d037c77ee90304
Oron Paz <oron.paz26@gmail.com>
2023-10-31 19:08:36 -0700

    changed to start instead of dev

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const { config } = require('dotenv');
 config();
-const fs = require('node:fs');
+const fs = require('fs');
 const OpenAI = require("openai");
 const express = require('express');
 const path = require('path');
